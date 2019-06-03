69bb3266c95e45492cecdba2b484d9c5678efa89
Emerson Jordan and Lucas Blasdel <emerson+lucas@>
2019-06-03 10:01:22 -0700

    calculate bmi

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
 
 var add = function(number1,number2) {
   return number1 + number2;
@@ -17,7 +17,20 @@ var divide = function (number1, number2){
   return number1 / number2;
 }
 
-var result = alert(add(number1, number2));
-var result = alert(subtract(number1, number2));
-var result = alert(multiply(number1, number2));
-var result = alert(divide(number1, number2));
+// var result = alert(add(number1, number2));
+// var result = alert(subtract(number1, number2));
+// var result = alert(multiply(number1, number2));
+// var result = alert(divide(number1, number2));
+
+var w = parseInt(prompt("what is your weight"));
+var h = parseInt(prompt("what is your height"));
+
+function bmi(weight, height){
+  var bmi = (weight * 703) / height;
+  return bmi;
+}
+
+alert(bmi(w,h));
+
+
+// var bmiCalc = bmi(prompt("what is your weight")),parseInt(prompt("what is your height")));
